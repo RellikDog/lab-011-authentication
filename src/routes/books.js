cc82664d2f74bbb522990a26be70485550223123
RickBell <richard.belamy123@gmail.com>
2019-03-12 20:59:29 -0700

    for night

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,10 +1,29 @@
 'use strict';
 
-module.exports = (req,res,next) => {
-    let error = { error: 'Resource Not Found' };
-    res.statusCode = 404;
-    res.statusMessage = 'Not Found';
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(error));
-    res.end();
-};
+const express = require('express');
+const router = express.Router();
+
+router.get('/books', handleGetAll);
+router.get('/books/:id', handleGetOne);
+
+// Route Handlers
+function handleGetAll(req, res, next) {
+    let books = {
+        count: 3,
+        results: [
+            { title:'Moby Dick' },
+            { title:'Little Women' },
+            { title: 'Eloquent Javascript' },
+        ],
+    };
+    res.status(200).json(books);
+}
+
+function handleGetOne(req, res, next) {
+    let book = {
+        title:'Moby Dick',
+    };
+    res.status(200).json(book);
+}
+
+module.exports = router;
