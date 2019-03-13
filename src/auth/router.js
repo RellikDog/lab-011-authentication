c07a0d43a94fa8a7d8d131cefbb07ef129bd6fb8
Connor Sihon <Connor@users-MBP.localdomain>
2019-03-12 21:01:14 -0700

    tue EOD

--- src/auth/router.js
+++ src/auth/router.js
@@ -6,6 +6,13 @@ const authRouter = express.Router();
 const User = require('./users-model.js');
 const auth = require('./middleware.js');
 
+/**
+ * this function signs a user up by creating a new user, saving them to the db, creating a token and sending it back
+ * @post
+ * @param {ref} req - the request that the user has sent to the api.
+ * @param {ref} res - the places to return the data to after the request is completed.
+ * @param {fn} next - the next function in the path.
+ */
 authRouter.post('/signup', (req, res, next) => {
     let user = new User(req.body);
     user.save()
