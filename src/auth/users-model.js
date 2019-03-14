4471d8dd9783d987ea84b907b0017766d8a937b4
Connor Sihon <Connor@users-MBP.localdomain>
2019-03-13 20:05:26 -0700

    added in lab doc

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const users = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
